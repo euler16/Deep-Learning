7d0d3cac67284fd9c5e08e4518075386aa9186b5
euler16 <nilayshrivastava1729@gmail.com>
2016-12-20 20:54:56 +0530

    working

--- tf1.py
+++ tf1.py
@@ -1,8 +1,24 @@
 import tensorflow as tf
 
-x = tf.placeholder("float",None)
-y = x**2
+# x = tf.placeholder("float",None)
+# y = x**2
 
-with tf.Session() as session:
-	result = session.run(y,feed_dict = {x:[1,2,3]})
-	print(result)
+# with tf.Session() as session:
+# 	result = session.run(y,feed_dict = {x:[1,2,3]})
+# 	print(result)
+
+# a = tf.constant([2])
+# b = tf.constant([3])
+
+a = tf.constant(2)
+b = tf.constant(3)
+
+c = a + b
+d = tf.add(a,b)
+
+session = tf.Session()
+result = session.run(c)
+result1 = session.run(d)
+
+print(result,end = " ")
+print(result1)
